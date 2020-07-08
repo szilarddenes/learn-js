8674def3f262788f741ceffac9cf1495341041d7
szdenes <57998732+szdenes@users.noreply.github.com>
2020-07-08 17:53:53 +0200

    todo-app CRUD done nac

--- todo-app/public/browser.js
+++ todo-app/public/browser.js
@@ -20,12 +20,10 @@ document.addEventListener('click', (e) => {
     //e.target = the target of the clicked item---if contains the #id
     if (e.target.classList.contains('edit-me')) {
         let userInput = prompt(
-                'Enter ur new text:',
-                e.target.parentElement.parentElement.querySelector('.item-text').innerHTML
-            )
-            //post a = url, post b = data it's going to be sent to the url --will return a promise --for not known time interval is useful --on the fly.
+            'Enter ur new text:',
+            e.target.parentElement.parentElement.querySelector('.item-text').innerHTML
+        )
 
-        //then()--it's not going to run until the post is not ready or action is not complete
         if (userInput) {
             axios
                 .post('/update-item', {
